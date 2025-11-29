857e98c144595d325c5ce8b3abdc027da822805d
choigilung <rlfdnd6914@naver.com>
2025-11-29 20:08:48 +0900

    윈도우 체계에서 효과음 설정

--- nuguri.c
+++ nuguri.c
@@ -154,6 +154,25 @@ void load_maps() {
     fclose(file);
 }
 
+void play_sound(int type) {
+#ifdef _WIN32 //windows의 beep함수 사용
+//주파수, 지속시간 조정
+    if (type == SOUND_COIN) {
+        Beep(800, 40);
+    } else if(type == SOUND_HIT) {
+        Beep(170, 130);
+    } else if(type == SOUND_CLEAR) {
+        Beep(900, 50);
+        Beep(1200, 60);
+    } else if(type == SOUND_JUMP) {
+        Beep(500, 30);
+    } else if(type == SOUND_DEAD) {
+        Beep(180, 600);
+    }
+
+
+}
+
 // 현재 스테이지 초기화
 void init_stage() {
     enemy_count = 0;
